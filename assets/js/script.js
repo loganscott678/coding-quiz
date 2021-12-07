b6ef5484907f3acc11834e83a117edce6e898d45
loganscott678 <93100495+loganscott678@users.noreply.github.com>
2021-12-07 15:56:51 -0600

    initial html

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// set a timer by using setInterval() 
+
+// if timer hits 0, end quiz
+
+// subtract points/time for inaccurate responses
+
+// set a variable to keep track of time 
+
+// iterate through each question and have each question replace previous 
+// - "if 'this' is clicked, hide() this, show() that"
+// - * refer to each section class for each question
+
+// keep score, save to localStorage and pull in to display-scores
+// - saveQuiz() function
+
+// if statement for correct answer validation 
+
